59eeee6719a5f991eb9fbe31e3b61bc724016366
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-08-28 11:22:48 -0300

    Initial Commit

---
+++ ModalSBTests/ModalSBTests.h
@@ -0,0 +1,13 @@
+//
+//  ModalSBTests.h
+//  ModalSBTests
+//
+//  Created by Rafael Brigagão Paulino on 28/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ModalSBTests : SenTestCase
+
+@end
---
+++ ModalSBTests/ModalSBTests.m
@@ -0,0 +1,32 @@
+//
+//  ModalSBTests.m
+//  ModalSBTests
+//
+//  Created by Rafael Brigagão Paulino on 28/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import "ModalSBTests.h"
+
+@implementation ModalSBTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ModalSBTests");
+}
+
+@end
